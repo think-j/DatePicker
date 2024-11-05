be20cfd01da7c61f0ea9fdc1b3c2aae0879ac001
think-j <flame2080@naver.com>
2024-11-05 16:43:01 +0900

    test

--- DatePicker/ViewController.swift
+++ DatePicker/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-var test = 1
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
